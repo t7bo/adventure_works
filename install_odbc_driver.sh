a6a6673fefdd2d57fc0d4f8485a06b380e01f1f1
t7bo <tibo7w@gmail.com>
2024-11-25 14:37:07 +0100

    ODBC driver update (works on linux)

--- install_odbc_driver.sh
+++ install_odbc_driver.sh
@@ -1,30 +1,24 @@
-#!/bin/bash
+if ! [[ "18.04 20.04 22.04 23.04 24.04" == *"$(lsb_release -rs)"* ]];
+then
+    echo "Ubuntu $(lsb_release -rs) is not currently supported.";
+    exit;
+fi
 
-# Script pour installer automatiquement le pilote ODBC Microsoft pour SQL Server
+# Add the signature to trust the Microsoft repo
+# For Ubuntu versions < 24.04 
+curl https://packages.microsoft.com/keys/microsoft.asc | sudo tee /etc/apt/trusted.gpg.d/microsoft.asc
+# For Ubuntu versions >= 24.04
+curl https://packages.microsoft.com/keys/microsoft.asc | sudo gpg --dearmor -o /usr/share/keyrings/microsoft-prod.gpg
 
-# Mise à jour des paquets
-echo "Mise à jour des paquets..."
-sudo apt-get update -y
-
-# Ajoute la clé de Microsoft pour les paquets
-echo "Ajout de la clé Microsoft..."
-curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
-
-# Ajoute le dépôt Microsoft pour la version d'Ubuntu
-echo "Ajout du dépôt Microsoft pour Ubuntu..."
+# Add repo to apt sources
 curl https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/prod.list | sudo tee /etc/apt/sources.list.d/mssql-release.list
 
-# Met à jour les sources des paquets
-echo "Mise à jour des sources des paquets..."
-sudo apt-get update -y
-
-# Accepte la licence et installe le pilote ODBC pour SQL Server
-echo "Installation du pilote ODBC Microsoft pour SQL Server..."
+# Install the driver
+sudo apt-get update
 sudo ACCEPT_EULA=Y apt-get install -y msodbcsql18
-
-# Vérifie que le pilote est installé
-echo "Vérification de l'installation du pilote ODBC..."
-odbcinst -q -d
-
-# Installation réussie
-echo "Le pilote ODBC Microsoft pour SQL Server a été installé avec succès."
+# optional: for bcp and sqlcmd
+sudo ACCEPT_EULA=Y apt-get install -y mssql-tools18
+echo 'export PATH="$PATH:/opt/mssql-tools18/bin"' >> ~/.bashrc
+source ~/.bashrc
+# optional: for unixODBC development headers
+sudo apt-get install -y unixodbc-dev
